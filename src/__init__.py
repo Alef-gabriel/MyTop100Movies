d9ccde52d728eb968ca858ae51ed15228e2b8292
alef gabriel <alefff.gabriel@gmail.com>
2020-07-02 13:44:11 -0300

    get rote and serialize Models

--- src/__init__.py
+++ src/__init__.py
@@ -2,14 +2,20 @@ from flask import Flask
 from .blueprint.bp import bp
 from .models.mod import configure as config_db
 from flask_migrate import Migrate
+from .ma import configure as config_ma
 
 #Function for create app. best sintax
 def create_app():
     app = Flask(__name__)
+    #app config
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Movies.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SQLALCHEMY_ECHO'] = True
     #db configurations and migrations
     config_db(app)
     Migrate(app,app.db)
+    config_ma(app)
 
     app.register_blueprint(bp)
+    
     return app
--- src/blueprint/bp.py
+++ src/blueprint/bp.py
@@ -1,10 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, json, jsonify
+from src.models.mod import db , Movies
+from src.ma import MoviesSchema
 
 #Create blueprint
 bp = Blueprint('bp', __name__)
 
 #first route
-@bp.route('/')
-def index():
-    return ''
+@bp.route('/Movies', methods =['GET'])
+def get_all():
+    bs = MoviesSchema(many=True)
+
+    movies = Movies.query.all()
+
+    return bs.jsonify(movies)
 
